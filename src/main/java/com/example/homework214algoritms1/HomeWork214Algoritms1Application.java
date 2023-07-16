0202650a2c447f504a66bc96ca6979427eb3a41d
ranss@rambler.ru <ranss@rambler.ru>
2023-07-16 13:37:15 +0300

    init commit

---
+++ src/main/java/com/example/homework214algoritms1/HomeWork214Algoritms1Application.java
@@ -0,0 +1,13 @@
+package com.example.homework214algoritms1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork214Algoritms1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork214Algoritms1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework214algoritms1/HomeWork214Algoritms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework214algoritms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork214Algoritms1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
